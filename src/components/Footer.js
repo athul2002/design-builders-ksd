954f71243a0a5711c0d11323e7351160f24857a8
Athul <athulmaliyakkal@gmail.com>
2022-06-29 13:04:57 +0530

    Modified Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MailIcon from '@mui/icons-material/Mail'
+import CallIcon from '@mui/icons-material/Call'
 import WhatsappIcon from '@mui/icons-material/WhatsApp'
 import '../styles/Footer.css'
 
@@ -7,13 +8,16 @@ function Footer() {
   return (
     <div className='Footer'>
         <div className='SocialMedia'>
-
+        <a href='tel:9447653632' target='_blank' rel="noreferrer">
+        <CallIcon/>
+        </a> 
         <a href='mailto:designbuilderskkl@gmail.com' target='_blank' rel="noreferrer">
         <MailIcon/>
         </a>   
         <a href='https://wa.me/9447653632' target='_blank' rel="noreferrer">
          <WhatsappIcon/>   
          </a>
+         
          <p> &copy;2022 designbuilderskkl.com </p>
 
         </div>
