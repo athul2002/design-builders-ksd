4d080b2348745ca770eed05ac9130bdc189d9b0d
Athul <athulmaliyakkal@gmail.com>
2022-06-29 12:50:02 +0530

    Modified Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import FacebookIcon from '@mui/icons-material/Facebook'
 import MailIcon from '@mui/icons-material/Mail'
 import WhatsappIcon from '@mui/icons-material/WhatsApp'
@@ -9,9 +10,12 @@ function Footer() {
     <div className='Footer'>
         <div className='SocialMedia'>
 
-         <FacebookIcon/>
-         <MailIcon/>   
+        <a href='mailto:designbuilderskkl@gmail.com' target='_blank'>
+        <MailIcon/>
+        </a>   
+        <a href='https://wa.me/9447653632' target='_blank'>
          <WhatsappIcon/>   
+         </a>
          <p> &copy;2022 designbuilderskkl.com </p>
 
         </div>
